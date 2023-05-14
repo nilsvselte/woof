e2bd31c5be275747156b7ffce65efb49654c48e0
Nils Selte <nilsvs@stud.ntnu.no>
2023-05-14 19:13:13 +0200

    cahnged max memory usage to user limit 1024mb added size limit of 200mb

--- pages/api/doggo/getdoggo.js
+++ pages/api/doggo/getdoggo.js
@@ -8,9 +8,17 @@ export const config = {
 
 export default async function handler(req, res) {
   try{
-    console.log("Fetching dog")
-    const randoDog = await fetch("https://random.dog/woof.json");
-    const randoDogURL = await randoDog.json();
+    console.log("Fetching dog");
+    //TODO:add check for filesize over 1024mb if that is the case ask again
+    let randoDog = await fetch("https://random.dog/woof.json");
+    let randoDogURL = await randoDog.json();
+    //refetch image link if filesize is over 200mb for improved speed and reliability
+    console.log(randoDogURL.fileSizeBytes);
+    while(randoDogURL.fileSizeBytes>200000000){
+        randoDog = await fetch("https://random.dog/woof.json");
+        randoDogURL = await randoDog.json();
+        console.log("refetching randodog URL")
+    }
     const response = await fetch(randoDogURL.url);
     const arrayBuffer = await response.arrayBuffer();
     const base64 = Buffer.from(arrayBuffer).toString("base64");
