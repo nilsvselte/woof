a09b8c7eff8265247753d430d095921c14b117d1
Nils Selte <nilsvs@stud.ntnu.no>
2023-05-14 18:59:01 +0200

    added vercel.json file and included larger memory limit on the serverless function to resolve 413 errors

--- pages/api/doggo/getdoggo.js
+++ pages/api/doggo/getdoggo.js
@@ -20,6 +20,6 @@ export default async function handler(req, res) {
     res.status(200).json({ image: base64, isMovie: isMovie });
   } catch (error){
     console.warn(error)
-    res.status(500).json({error: error})
+    res.status(500).json({error: error.message})
   }
 }
